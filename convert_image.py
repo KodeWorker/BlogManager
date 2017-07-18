d4eef1fd5040eead64c26d2d05ff07cf7199a9ff
shinfuwu <shinfuwu@itri.org.tw>
2017-07-18 09:08:14 +0800

    add comments

--- convert_image.py
+++ convert_image.py
@@ -1,34 +1,43 @@
-# -*- coding: utf-8 -*-
+""" Convert All Files in PIXNET Album into .JPG Format
+# Description:
+    After downloading all the pictures in PIXNET album, we need to get rid of generated numbers and convert the formate for smaller storage capacity.
+# Author: Shin-Fu (Kelvin) Wu
+# Date: 2017/07/18
+# Reference:
+    - https://sofree.cc/bloginwp-pixnet-album/
+    - http://laby2.blogspot.com/
+    - http://pillow.readthedocs.io/en/3.0.x/handbook/image-file-formats.html
 """
-Created on Mon Jul 17 12:31:55 2017
-
-@author: ITRI-A50117
-"""
-
 import os
 from shutil import copyfile
 from PIL import Image
 from PIL import ImageFile
 
+# Avoid "image truncated: error
 # https://stackoverflow.com/questions/12984426/python-pil-ioerror-image-file-truncated-with-big-images
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 if __name__ == '__main__':
-    
-    album_path = 'C:/album/kwbuster'
+
+    # PIXNET album path
+    album_path = 'path/to/album'
+    # path of converted album
     convert_path = os.path.join(os.path.dirname(__file__),'images')
-    
+
+    # search for sub-albums in the album_path
     image_dirs = [x[1] for x in os.walk(album_path)][0]
     for image_dir in image_dirs:
         ind = image_dir.index('-') + 1
-        
+
         if not os.path.exists(convert_path + '/' + image_dir[8:]):
             os.makedirs(convert_path + '/' + image_dir[ind:])
-        
+
+        # search for files in the album_path
         files = [x[2] for x in os.walk(album_path + '/' + image_dir)][0]
         for file in files:
             idx = file.index('-') + 1
-            
+
+            # image convertion
             if not file.endswith('.jpg'):
                 im = Image.open(album_path + '/' + image_dir + '/' + file)
                 im.convert('RGB').save(convert_path + '/' + image_dir[ind:] + '/%s.jpg' %(file[idx:-4]))
--- relocate_pixnet_blog.py
+++ relocate_pixnet_blog.py
@@ -1,9 +1,16 @@
+""" Convert All Files in PIXNET Album into .JPG Format
+# Description:
+    Build the .md files from my PIXNET exported blog backup file.
+# Author: Shin-Fu (Kelvin) Wu
+# Date: 2017/07/14
+"""
 import os
 
 def generate_post(init_ind, end_ind, file_list):
     file_content = file_list[init_ind:end_ind - 1]
     title = file_list[init_ind].decode("utf-8")[7:-2]
-    
+
+    # Get rid of special characters from filename
     if ']' in title:
         title = title[title.index(']')+1:]
     title = title.replace(' - ', '-')
@@ -17,7 +24,7 @@ def generate_post(init_ind, end_ind, file_list):
     title = title.replace(')', '')
     title = title.replace('、', '-')
     title = title.replace('~', '-')
-    
+
     date = file_list[init_ind+2].decode("utf-8")[6:16]
     file_name = '%s-%s-%s-%s.md' %(date[-4:], date[:2], date[-7:-5], title)
     return file_content, file_name
@@ -26,10 +33,10 @@ def generate_metadata(metadata):
     output_metadata = []
     output_metadata.append(b'---\n')
     output_metadata.append(b'layout: single\n')
-    
+
     for line in metadata:
         if 'TITLE'.encode('utf-8') in line:
-            
+
             title = str(line, 'utf-8')
             if ']' in title:
                 title = title[title.index(']')+1:]
@@ -44,7 +51,7 @@ def generate_metadata(metadata):
 #            title = title.replace(')', '')
 #            title = title.replace('、', '-')
 #            title = title.replace('~', '-')
-            
+
             output_metadata.append(b'title: %s' %title.encode('utf-8'))
         if 'DATE'.encode('utf-8') in line:
             if 'AM'.encode('utf-8') in line:
@@ -70,7 +77,7 @@ def generate_metadata(metadata):
 
 def generate_content(content):
     output_content = []
-    for line in content:        
+    for line in content:
         if '&nbsp;'.encode('utf-8') in line:
             pass
         elif '<p>'.encode('utf-8') in line:
@@ -83,10 +90,10 @@ def generate_content(content):
     return output_content
 
 if __name__ == '__main__':
-    
+
     """Section 1: Setting File and Path"""
     ###########################################################################
-    
+
     file_path = os.path.join(os.path.dirname(__file__), 'pixnet_backup', 'blog-export-mt-KWbuster-20170714081020.txt')
     post_dir = os.path.join(os.path.dirname(__file__), 'posts')
 
@@ -97,13 +104,13 @@ if __name__ == '__main__':
     ###########################################################################
     with open(file_path, 'rb') as read_file:
         file_list = read_file.readlines()
-    
+
     init_ind, end_ind = 0, 0
     while init_ind < len(file_list) and end_ind < len(file_list):
         # Divide posts by "TITLE"
         if (end_ind != 0) and ('TITLE'.encode('utf-8') in file_list[end_ind]):
             file_content, file_name = generate_post(init_ind, end_ind, file_list)
-            
+
             # Write the post
             if 'publish'.encode('utf-8') in file_content[5]:
                 print('process... %s' %file_name)
@@ -111,33 +118,33 @@ if __name__ == '__main__':
                     write_file.writelines(file_content)
             init_ind = end_ind
         end_ind += 1
-    
+
     # last post
     file_content, file_name = generate_post(init_ind, end_ind, file_list)
-    
+
     if 'publish'.encode('utf-8') in file_content[5]:
         print('process... %s' %file_name)
         with open(post_dir + '/' + file_name, 'wb') as write_file:
             write_file.writelines(file_content)
-    
+
     """Section 3: Organize Markdown Files"""
     ###########################################################################
-    
+
     post_files = [x[2] for x in os.walk(post_dir)][0]
     for post_file in post_files:
         with open(post_dir + '/' +post_file, 'rb') as read_file:
             post_content = read_file.readlines()
-        
+
         print('Markdown...%s' %post_file)
         # Divide the post into metadata and content parts
         for ind in range(len(post_content)):
             if 'BODY'.encode('utf-8') in post_content[ind]:
                 metadata = post_content[:ind]
                 content = post_content[ind+1:-1]
-        
+
         # Jekyll Markdown
         output_metadata = generate_metadata(metadata)
-        output_content = generate_content(content)        
-        
+        output_content = generate_content(content)
+
         with open(post_dir + '/' +post_file, 'wb') as write_file:
             write_file.writelines(output_metadata+output_content)
