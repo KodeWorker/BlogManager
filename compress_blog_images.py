15c5d1aeb3df76792a093483dcf8c769dedf2bd1
Kelvin Wu(吳信夫) <Kelvin_Wu@corpnet.asus>
2020-03-06 15:37:15 +0800

    20200306 update

---
+++ compress_blog_images.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+import os
+from shutil import rmtree, copyfile
+from PIL import Image
+import math
+
+if __name__ == "__main__":
+    
+    source_dir = "../KodeWorker.github.io/assets/images"
+    
+    target_dir = "images"
+    limit = 1024
+    
+    if os.path.exists(target_dir):
+        rmtree(target_dir)
+    os.makedirs(target_dir)
+    
+    for dirPath, dirNames, fileNames in os.walk(source_dir):
+        for fileName in fileNames:
+            if fileName.lower().endswith(".jpg") or\
+            fileName.lower().endswith(".png"):
+            
+                source_path = os.path.join(dirPath, fileName)
+                dirname = os.path.dirname(source_path)
+                dirname = dirname.replace(source_dir, target_dir)
+                if not os.path.exists(dirname):
+                    os.makedirs(dirname)
+                target_path = os.path.join(dirname, fileName)
+                
+                # Image Compression                
+                img = Image.open(source_path)
+                if max(img.size) >= limit:
+                    h = img.size[0]
+                    w = img.size[1]
+                    if h >= w:
+                        ratio = 1024/h
+                    else:
+                        ratio = 1024/w
+                    size = math.ceil(h*ratio), math.ceil(w*ratio)
+                    
+                    img.thumbnail(size, Image.ANTIALIAS)
+                img.save(target_path, optimize=True, quality=95)
+            
+            elif fileName.lower().endswith(".gif"):
+                
+                source_path = os.path.join(dirPath, fileName)
+                dirname = os.path.dirname(source_path)
+                dirname = dirname.replace(source_dir, target_dir)
+                if not os.path.exists(dirname):
+                    os.makedirs(dirname)
+                target_path = os.path.join(dirname, fileName)
+                
+                copyfile(source_path, target_path)
--- readme.md
+++ readme.md
@@ -3,3 +3,4 @@
 
 ### 內容
 - **relocate_pixnet_blog.py** 將痞客邦備份文件，依照每一篇文章建立檔案並轉成markdown格式。
+- **compress_blog_images.py** 將個人網頁中的圖像進行備份並壓縮
